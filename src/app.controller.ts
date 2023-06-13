1fafdeb01c7d840a893b6c67656f1e022ef211f9
zhaozhenghao.0 <975106543@qq.com>
2023-06-13 11:10:53 +0800

    feat: @Optional 表示可选参数 默认值为null

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject, Optional } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // 方式一
+  // constructor(private readonly appService: AppService) {}
+  // 方式二
+  /* @Inject(AppService)
+  private readonly appService: AppService */
+  // 方式三
+  // constructor(@Inject('app_service') private readonly appService: AppService) {}
+  /* constructor(@Inject('app_service') private readonly appService: AppService,
+  @Inject('person') private readonly person: {name:string,age:number},
+  @Inject('person2') private readonly person2: {name:string,age:number},
+  // @Inject('person3') private readonly person3: {name:string,age:number},
+  ) { } */
+  // 方式四 标注在类或者方法参数上，表示该参数可选，不是必需的
+  @Optional()
+  constructor(@Optional() private appService:AppService) {
+    
+  })
 
   @Get()
   getHello(): string {
-    debugger;
+    // console.log('person3',this.person2);
+    
+    // debugger;
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,47 @@ import { AppService } from './app.service';
 import { AtestModule } from './atest/atest.module';
 import { XxxModule } from './xxx/xxx.module';
 import { PersonModule } from './person/person.module';
+import { Person } from './person/entities/person.entity';
 
 @Module({
   imports: [AtestModule, XxxModule, PersonModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController], //控制器负责处理传入的请求和向客户端返回响应。
+  // providers:[AppService],//方式一 绑定controller.ts
+  /* providers: [ // 方式二 绑定controller.ts
+    {
+      provide: AppService, //指定注入的 token
+      useClass: AppService, //指定注入的对象的类
+    },
+  ], */
+  providers: [ // 方式三
+    {
+      provide: 'app_service', //指定注入的 token
+      useClass: AppService, //指定注入的对象的类
+    }, {
+      provide: 'person',
+      useValue: {// 静态属性
+        name: 'aaa',
+        age:18
+      }
+    },
+    {
+      provide: 'person2',
+      useFactory() { // 动态属性
+        return {
+          name: '小明',
+          age:90
+        }
+      }
+    },
+    /* {
+      provide: 'person3',
+      useFactory(person:{name:string},appService:AppService) { // 动态属性
+        return {
+          name: person.name,
+          desc:appService.getHello()
+        }
+      }
+    } */
+  ]
 })
 export class AppModule {}
